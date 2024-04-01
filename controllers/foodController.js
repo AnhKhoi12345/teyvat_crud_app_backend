b560211fe49ec9d61e192820956f75567d8c6bf2
Bachiro <bachvo01@gmail.com>
2024-04-01 22:48:39 +0700

    fixed cannot convert object to primitive value

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -38,25 +38,19 @@ const updateFood = async (req, res) => {
   try {
     const { id } = req.params;
     
-    // req.body is the data you type in 
     const body = req.body;
-
-    // because you upload the image as a file, it is not considered in the req.body (request body) but in the req.file (request file)
     const fileName = req.file.filename;
-
     const toBeUpdatedFood = Object.assign(body, {image: fileName});
-    console.log(toBeUpdatedFood);
 
-    // check the console log to understand it better
-    // req.body only contains name, price except the image file
-    // req.file contains filename, file size, etc.
-    console.log("req.body : " + req.body);
-    console.log("req.file: " + req.file);
-    const food = await Food.findByIdAndUpdate(id, req.body);
+    console.log(toBeUpdatedFood)
+    console.log(req.body);
+    console.log(req.file);
+    const food = await Food.findByIdAndUpdate(id, toBeUpdatedFood);
     if (!food) {
       return res.status(404).json({ message: "food not found" });
     }
     const updatedFood = await Food.findById(id);
+    console.log(updatedFood)
     res.status(200).json(updatedFood);
   } catch (error) {
     res.status(500).json({ message: error.message });
